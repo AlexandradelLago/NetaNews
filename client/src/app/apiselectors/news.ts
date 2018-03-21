a46ab236ff0f91451406c70dc106285024342994
alexandradelLago <daviniaroca@gmail.com>
2018-03-21 10:11:26 -0700

    past is present now

--- client/src/app/apiselectors/news.ts
+++ client/src/app/apiselectors/news.ts
@@ -34,8 +34,7 @@ country:[
     "ae",
     "ar",
     "mx",
-    "br",
-    "it"
+    "br"
 //   ar at au be bg  ca ch cn co cu cz de eg fr gb gr hk hu id ie il in 
 //   jp kr lt lv ma  my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za
 ]
