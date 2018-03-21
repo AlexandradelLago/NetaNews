707312016d0093d3eb4135647e48c9f99e00aad3
alexandradelLago <daviniaroca@gmail.com>
2018-03-21 08:56:19 -0700

    news selector created

--- client/src/app/apiselectors/news.ts
+++ client/src/app/apiselectors/news.ts
@@ -1,35 +1,63 @@
 
- const categories:Array<string> =[
+ const news:Object ={
+     header:[
+       {acronim:"headlines",show:"",logo:''},
+       {acronim:"everything",show:"",logo:''},
+       {acronim:"sources",show:"",logo:''} 
+     ],
+     categories:[
     "business",
     "entertainment",
     "general",
-    "health",
-    "science",
     "sports",
-    "technology",
+    "technology"
+   ],
+languages:[
+   // { acronim:"de",word:  } ",
+    { acronim:"en",language:"English"} ,
+    { acronim:"es",language:"Spanish"} ,
+],
+country:[
+    {acronim:"ar",country:"Argentina",logo:'',continent:"Am"},
+    {acronim:"br",country:"Brazil",logo:'',continent:"Am"},
+    {acronim:"co",country:"Colombia",logo:'',continent:"Am"},
+    {acronim:"mx",country:"Mexico",logo:'',continent:"Am"},
+    {acronim:"fr",country:"France",logo:'',continent:"Eu"},
+    {acronim:"gb",country:"United Kingdom",logo:'',continent:"Eu"},
+    {acronim:"ph",country:"Philippines",logo:'',continent:"As"},
+    {acronim:"us",country:"USA",logo:'',continent:"Am"},
+    {acronim:"fr",country:"France",logo:'',continent:"Eu"},
+    {acronim:"de",country:"Germany",logo:'',continent:"Eu"},
+    {acronim:"ru",country:"Rusia",logo:'',continent:"Eu"}
+],
+sources:[
+    {acronim:"abc-news",language:"en",logo:'',category:"general"},
+    {acronim:"the-new-york-times",language:"en",logo:'',category:"general"},
+    {acronim:"the-huffington-post",language:"en",logo:'',category:"general"},
+    {acronim:"the-washington-post",language:"en",logo:'',category:"general"},
+    {acronim:"fox-news",language:"en",logo:'',category:"general"},
+    {acronim:"bbc-news",language:"en",logo:'',category:"general"},
+    {acronim:"bleacher-report",language:"en",logo:'',category:"entertainment"},
+    {acronim:"cbs-news",language:"en",logo:'',category:"general"},
+    {acronim:"daily-mail",language:"en",logo:'',category:"general"},
+    {acronim:"cnn",language:"en",logo:'',category:"general"},
+    {acronim:"cnn-es",language:"es",logo:'',category:"general"},
+    {acronim:"buzzfeed",language:"en",logo:'',category:"entertainment"},
+    {acronim:"mashable",language:"en",logo:'',category:"entertainment"},
+    {acronim:"mtv-news",language:"en",logo:'',category:"entertainment"},
+    {acronim:"vice-news",language:"en",logo:'',category:"entertainment"},
+    {acronim:"spiegel",language:"en",logo:'',category: "general"},
+    {acronim:"bbc-sport",language:"en",logo:'',category:"sports"},
+    {acronim:"espn",language:"en",logo:'',category:"sports"},
+    {acronim:"fox-sport",language:"en",logo:'',category:"sports"},
+    {acronim:"marca",language:"en",logo:'',category:"sports"},
+    {acronim:"nfl-news",language:"en",logo:'',category:"sports"},
+    {acronim:"ign",language:"en",logo:'',category:"technology"},
+    {acronim:"techradar",language:"en",logo:'',category:"technology"},
+    {acronim:"business-insider",language:"en",logo:'',category:"business"},
+    {acronim:"the-wall-street-journal",language:"en",logo:'',category:"business"},
+    {acronim:"wired",language:"en",logo:'',category:"technology"},
+    {acronim:"bloomberg",language:"en",logo:'',category:"business"}
 ]
-
-export default categories;
-
-// languages
-const languages:Array<string>=[
-    "ar",
-     "de",
-      "en",
-       "es",
-        "fr",
-         "he",
-          "it",
-           "nl",
-            "no",
-             "pt",
-              "ru",
-               "se",
-                "ud", 
-                    "zh"
-]
-
-// country
-// Find sources that display news in a specific country. Possible options:
-//  ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it
-//   jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za
+}
+export default news;
